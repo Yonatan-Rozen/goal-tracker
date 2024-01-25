63c3eef6e3634a880e7e097e37584ff5170e45ac
Yonatan Rozen <yon969@outlook.co.il>
2024-01-26 00:31:04 +0200

    Added Footer component

--- src/App.js
+++ src/App.js
@@ -219,3 +219,17 @@ function FormAddTask({ onAddTask }) {
     </div>
   );
 }
+
+function Footer() {
+  return (
+    <footer>
+      <h3>
+        Made By{" "}
+        <a href="https://github.com/Yonatan-Rozen">
+          Yonatan Rozen
+          <i className="fab fa-github"></i>
+        </a>
+      </h3>
+    </footer>
+  );
+}
